dc4475d6c9ec8beff1810bf1a832b4f8c297f541
Padrie <pstrehlow007@gmail.com>
2025-06-12 10:39:12 +0200

    a

--- Assets/+++Workdata/Scripts/Characters/Enemy/EnemyAI.cs
+++ Assets/+++Workdata/Scripts/Characters/Enemy/EnemyAI.cs
@@ -2,17 +2,23 @@ using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    [Header("Patrol")]
     public Transform playerTarget;
     public float waitTimeAtCheckpoint;
     public List<Transform> waypoints = new List<Transform>();
+    [Header("Chase")]
+    public float chasePlayerDelay;
 
+    NavMeshAgent agent;
     bool patrol = true;
 
     private void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         StartCoroutine(Patrol());
     }
 
@@ -31,10 +37,28 @@ public class EnemyAI : MonoBehaviour
 
     IEnumerator Patrol()
     {
+        int index = 0;
         while (patrol)
         {
+            if (waypoints.Count == 0) yield break;
+
+            Transform currentWaypoint = waypoints[index];
+            agent.SetDestination(currentWaypoint.position);
+
+            while (Vector3.Distance(transform.position, currentWaypoint.position) > 1)
+            {
+                yield return null;
+            }
+
             yield return new WaitForSeconds(waitTimeAtCheckpoint);
+
+            index = (index + 1) % waypoints.Count;
         }
+    }
+
+    IEnumerator FoundPlayer()
+    {
+        yield return new WaitForSeconds(chasePlayerDelay);
     }
 
     public void Chase()
